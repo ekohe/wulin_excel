fd07526263a484068c120b2026ae6d5171aa7ba7
Xuhao <xuhao@rubyfans.com>
2011-09-19 16:17:19 +0800

    refactoring

--- lib/wulin_excel.rb
+++ lib/wulin_excel.rb
@@ -1,27 +1,21 @@
+require 'wulin_excel/engine' if defined?(Rails)
+
 module WulinExcel
   MAXIMUM_NUMBER_OF_ROWS = 65535
-  
-  # rails 3.1 specified
-  class Engine < ::Rails::Engine
-    config.before_configuration do   
-      if !Object.const_defined?(:WulinMaster)
-        raise "WulinExcel needs WulinMaster. Make sure WulinExcel is loaded after WulinMaster configuring config.plugins properly in application.rb"
-      end
-    end
-  
-    config.before_initialize do 
-      # Add default export excel button to every grid
-      WulinMaster::Grid.add_to_default_toolbar "Excel", :class => 'excel_export', :icon => 'excel'
-
-      # Load excel javascript
-      WulinMaster::add_javascript 'excel.js'
-
-      # Register xls mime type
-      Mime::Type.register "application/vnd.ms-excel", :xls
-      
-      require 'writeexcel'
-      require 'wulin_excel/action'
-    end
-  end
-
 end
+
+if !defined?(WulinMaster)
+  raise "WulinExcel needs WulinMaster. Make sure WulinExcel is loaded after WulinMaster configuring config.plugins properly in application.rb"
+end
+
+# Add default export excel button to every grid
+WulinMaster::Grid.add_to_default_toolbar "Excel", :class => 'excel_export', :icon => 'excel'
+
+# Load excel javascript
+WulinMaster::add_javascript 'excel.js'
+
+# Register xls mime type
+Mime::Type.register "application/vnd.ms-excel", :xls
+
+require 'writeexcel'
+require 'wulin_excel/action'
---
+++ lib/wulin_excel/engine.rb
@@ -0,0 +1,8 @@
+require "wulin_excel"
+require "rails"
+
+module WulinExcel
+  class Engine < Rails::Engine
+    engine_name :wulin_excel
+  end
+end
