0e1f54593ede07cae7038192dc9c08990b675201
Xuhao <xuhao@ekohe.com>
2013-12-25 15:55:48 +0800

    MISC - Add excel_icon.png to precompile

--- lib/wulin_excel/engine.rb
+++ lib/wulin_excel/engine.rb
@@ -5,7 +5,7 @@ module WulinExcel
   class Engine < Rails::Engine
     engine_name :wulin_excel
     initializer "add assets to precompile" do |app|
-       app.config.assets.precompile += %w( excel.js excel.css )
+       app.config.assets.precompile += %w( excel.js excel.css excel_icon.png )
     end
   end
 end
