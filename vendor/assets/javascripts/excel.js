1d1146eb0c363ce22a4f22825dda4870f916afc9
mel <mel@ekohe.com>
2025-06-10 17:27:39 +0800

    https://gitlab.ekohe.com/ekohe/wulin/wulin_master/-/issues/300 - Excel Export to Match Displayed Column Order

--- vendor/assets/javascripts/excel.js
+++ vendor/assets/javascripts/excel.js
@@ -1,20 +1,15 @@
-// Excel Object definition
 (function($) {
   function Excel(gridName) {
-    var $grid;
-    var $paramsUrl;
-
-    function init() {
-      $grid = gridManager.getGrid(gridName);
-      $paramsUrl = '';
-    }
+    const grid = gridManager.getGrid(gridName);
+    let paramsUrl = "";
 
     function sendExcelRequest() {
-      if (($grid === null) || ($grid.loader === null))
+      if (!grid || !grid.loader) {
         return false;
+      }
 
       // Get columns order and size
-      addColumnSizeAndOrder();
+      collectColumnInformation();
 
       // Request download
       requestExcel();
@@ -22,109 +17,121 @@
       return true;
     }
 
-    function addColumnSizeAndOrder() {
-      $paramsUrl = '';
-      var arr = {};
-      var colArray = [];
-      var visible_columns = $grid.getColumns();
-      var all_columns = $grid.columns;
-      var export_columns = [];
+    // Collects column information for export
+    function collectColumnInformation() {
+      const visibleColumns = grid.getColumns();
+      const visibleColumnNames = visibleColumns.map(col => col["column_name"]);
+      let exportColumns = [];
 
-      const
-        visible_names = visible_columns.map(col => col['column_name']),
-        visible = col => visible_names.includes(col['column_name']),
-        excelExportUndefined = col => col['excel_export'] === undefined || col['excel_export'] === null,
-        excelExport = col => col['excel_export'] === true;
-      all_columns.forEach(function(column) {
-        if (visible(column)) {
-          if (excelExport(column) || excelExportUndefined(column)) export_columns.push(column);
-        } else {
-          if (excelExport(column)) export_columns.push(column);
+      // Filter columns for export
+      visibleColumns.forEach(function(column) {
+        const shouldExport = column["excel_export"] === true;
+        const hasNoExportSetting = column["excel_export"] === undefined || column["excel_export"] === null;
+        if (shouldExport || hasNoExportSetting) {
+          exportColumns.push(column);
         }
       });
 
-      $.each(export_columns, function(i, v){
-        arr[export_columns[i]['id']] = export_columns[i];
-      })
-      export_columns = [];
-      for(key in arr){
-        export_columns.push(arr[key]);
-      }
-
-      $.each(export_columns, function(index, value) {
-        colArray.push(value.id + '~' + value.width);
+      // Remove duplicates
+      const uniqueColumns = {};
+      exportColumns.forEach(function(column) {
+        uniqueColumns[column.id] = column;
       });
-      $paramsUrl += colArray.join();
+
+      exportColumns = Object.values(uniqueColumns);
+
+      // Format column data for URL
+      const columnPairs = exportColumns.map(column => `${column.id}~${column.width}`);
+      paramsUrl = columnPairs.join(",");
     }
 
+    // Displays a loading modal during export
     function displayLoadingModal() {
-      var $excelModal = Ui.baseModal()
-        .attr({'id': 'excel-modal'})
-        .width('500px');
+      const $excelModal = Ui.baseModal()
+        .attr({
+          "id": "excel-modal"
+        })
+        .width("500px");
 
-      $excelModal.find('.modal-content')
-        .append($('<h5/>').text('Excel export'))
-        .append($('<p/>').text('Please wait while your Excel document is being prepared.'))
-        .append($('<div/>').addClass('progress').append($('<div/>').addClass('indeterminate')))
+      $excelModal.find(".modal-content")
+        .append($("<h5/>").text("Excel export"))
+        .append($("<p/>").text("Please wait while your Excel document is being prepared."))
+        .append($("<div/>").addClass("progress").append($("<div/>").addClass("indeterminate")));
 
-      var $modalFooter = $('<div/>')
-        .addClass('modal-footer')
-        .append($('<div/>').addClass('btn modal-close').text('Close'))
+      $("<div/>")
+        .addClass("modal-footer")
+        .append($("<div/>").addClass("btn modal-close").text("Close"))
         .appendTo($excelModal);
     }
 
+    // Makes the AJAX request to generate and download Excel file
     function requestExcel() {
-      var screenName = $grid.screen;
-      var path = $grid.path +'.xlsx?' + $grid.query.replace('?', '') + $grid.loader.conditionalURI() + '&' + "columns=" + $paramsUrl;
+      const screenName = grid.screen;
+      const baseQuery = grid.query.replace("?", "");
+      const conditionalParams = grid.loader.conditionalURI();
+      const path = `${grid.path}.xlsx?${baseQuery}${conditionalParams}&columns=${paramsUrl}`;
+
+      displayLoadingModal();
 
       $.ajax({
         url: path,
-        type: 'GET'
+        type: "GET"
       }).success(function(status) {
-        $('#excel-modal').modal('close').remove();
-        if(status.file && status.name) {
-          var req = new XMLHttpRequest()
-          req.open("GET", `${$grid.path}.xlsx?filepath=${status.file}&filename=${status.name}&screen=${screenName}`, true)
-          req.responseType = "blob"
+        $("#excel-modal").modal("close").remove();
 
-          req.onload = function (event) {
-            var blob = req.response
-            var link = document.createElement('a')
-            link.href = window.URL.createObjectURL(blob)
-            link.download = status.name
-            link.click()
-
-            // Remove the resource
-            window.URL.revokeObjectURL(blob)
-            link.remove()
-          }
-          req.send()
+        if (status.file && status.name) {
+          downloadFile(status.file, status.name, screenName);
         }
         return false;
-      })
-      displayLoadingModal();
+      });
     }
 
-    init();
+    // Downloads the generated Excel file
+    function downloadFile(filepath, filename, screenName) {
+      const req = new XMLHttpRequest();
+      req.open("GET", `${grid.path}.xlsx?filepath=${filepath}&filename=${filename}&screen=${screenName}`, true);
+      req.responseType = "blob";
 
-    return {"sendExcelRequest": sendExcelRequest};
+      req.onload = function() {
+        const blob = req.response;
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = filename;
+        link.click();
+
+        // Remove the resource
+        window.URL.revokeObjectURL(blob);
+        link.remove();
+      };
+
+      req.send();
+    }
+
+    return {
+      sendExcelRequest
+    };
   }
-  $.extend(true, window, { Excel: Excel });
-})(jQuery);
 
+  // Add to global namespace
+  $.extend(true, window, {
+    Excel
+  });
+})(jQuery);
 
 // Export action
 WulinMaster.actions.Export = $.extend({}, WulinMaster.actions.BaseAction, {
-  name: 'export',
-
+  name: "export",
   handler: function() {
-    var grid = this.getGrid();
-    var excel = new Excel(grid.name);
+    const grid = this.getGrid();
+    const excel = new Excel(grid.name);
+
     if (!excel.sendExcelRequest()) {
       displayErrorMessage("Excel generation failed. Please try again later.");
     }
+
     return false;
   }
 });
 
+// Register the Export action
 WulinMaster.ActionManager.register(WulinMaster.actions.Export);
