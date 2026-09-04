229fb5ffaa10b6c2994091897633324ab583a9a4
Mel Cao <mel@ekohe.com>
2026-09-04 11:42:01 +0800

    Add Propshaft asset path registration with Sprockets fallback

--- lib/wulin_excel/engine.rb
+++ lib/wulin_excel/engine.rb
@@ -5,7 +5,11 @@ module WulinExcel
   class Engine < Rails::Engine
     engine_name :wulin_excel
     initializer "add assets to precompile" do |app|
-       app.config.assets.precompile += %w( excel.js excel.css excel_icon.png )
+      if defined?(Propshaft)
+        app.config.assets.paths << root.join("vendor", "assets", "images")
+      else
+        app.config.assets.precompile += %w( excel.js excel.css excel_icon.png )
+      end
     end
   end
 end
